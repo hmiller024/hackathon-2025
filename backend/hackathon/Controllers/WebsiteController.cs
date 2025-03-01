351e37a733aec0b4e1c735b30fe56c215661679c
cuper024 <hdmcuper@gmail.com>
2025-03-01 02:09:09 -0700

    might have made a filter

--- backend/hackathon/Controllers/WebsiteController.cs
+++ backend/hackathon/Controllers/WebsiteController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using HtmlAgilityPack;
 
 namespace Backend.Controllers
 {
@@ -41,6 +42,47 @@ namespace Backend.Controllers
             _logger.LogInformation("GETTING THE FUCKING WEBSITES BAYBE");
 
             var websites = await _context.TrackedWebsites.ToListAsync();
+            using (HttpClient client = new HttpClient())
+            {
+                foreach (var website in websites)
+                {
+                    try
+                    {
+                        // Check if the URL is a valid absolute URI
+                        Uri websiteUri;
+                        if (!Uri.TryCreate(website.Url, UriKind.Absolute, out websiteUri))
+                        {
+                            _logger.LogError($"Invalid URL: {website.Url} for website {website.Id}. Skipping...");
+                            website.ContentChanged = false; // Mark as false since the URL is malformed
+                            continue;  // Skip the website if the URL is invalid
+                        }
+
+                        // Make the HTTP request with the valid absolute URI
+                        HttpResponseMessage response = await client.GetAsync(websiteUri);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string htmlContent = await response.Content.ReadAsStringAsync();
+                            htmlContent = CleanHtml(htmlContent);
+                            if (htmlContent != website.LastHash)
+                            {
+                                website.ContentChanged = true;
+                                website.LastHash = htmlContent;
+                            }
+                        }
+                        else
+                        {
+                            website.ContentChanged = false;  // If request fails, mark as false
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Error fetching website {website.Url}: {ex.Message}");
+                        website.ContentChanged = false;  // In case of an error, mark as false
+                    }
+                }
+            }
+
             return Ok(websites);
         }
 
@@ -56,7 +98,7 @@ namespace Backend.Controllers
                     if (response.IsSuccessStatusCode)
                     {
                         string htmlContent = await response.Content.ReadAsStringAsync();
-
+                        htmlContent = CleanHtml(htmlContent);
                         TrackedWebsite website = new TrackedWebsite
                         {
                             Url = request.Url,
@@ -83,7 +125,28 @@ namespace Backend.Controllers
             }
 
         }
+        private string CleanHtml(string html)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var dynamicNodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'dynamic') or @id='timestamp']");
+
+            if (dynamicNodes != null)
+            {
+                foreach (var node in dynamicNodes)
+                {
+                    node.Remove(); // Remove unwanted elements
+                }
+            }
+
+            // Return the cleaned-up HTML as a string
+            return htmlDoc.DocumentNode.OuterHtml;
+        }
+
     }
+       
+
 
     public class WebsiteRequest
     {
--- backend/hackathon/Models/TrackedWebsites.cs
+++ backend/hackathon/Models/TrackedWebsites.cs
@@ -7,4 +7,5 @@ public class TrackedWebsite
     public string Name {  get; set; } = string.Empty ;
     public string LastHash { get; set; } = string.Empty;
     public DateTime LastChecked { get; set; } = DateTime.UtcNow;
+    public bool ContentChanged { get; set; }
 }
