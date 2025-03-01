2175530e7e6f95a87ca0371db14dfee53975a9b4
aysmith17 <aysmith17@gmail.com>
2025-03-01 05:22:43 -0700

    changes backend iteration

--- backend/hackathon/obj/Debug/net8.0/hackathon.AssemblyInfo.cs
+++ backend/hackathon/obj/Debug/net8.0/hackathon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hackathon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2fbc15762d8c098e59c195efdb35e8d8c13301")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48beabf19a517a6396ab652271fb3dce6066dec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("hackathon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hackathon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
